f2ba810c67cbb3ae381bd27ae3598c3ab7ac2600
aaron4pf <aaronsauceda12@gmail.com>
2022-11-03 15:25:38 -0700

    added looping time project
    added loops

---
+++ ScriptingProjectSaucedaP7/Assets/Scripts/DoWhileLoop.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoWhileLoop : MonoBehaviour
+{
+    void Start()
+    {
+        bool shouldContinue = false;
+        do
+        {
+            print("Hello World");
+        }
+        while (shouldContinue == true);
+    }
+}
